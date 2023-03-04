a9082bf45a4b47a33f2ce9375eac39951be3f578
Sybil Santos-Burgan <ssburgan@gmail.com>
2023-03-04 14:13:43 -0600

    adding selectors

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,11 @@
 
 
 const downloadButton = document.getElementById('download-button');
+const selectYear = document.getElementById('select-year');
+const selectedModel = document.querySelector( 'input[name="model"]:checked');   
 
+
+button.addEventListener('click', disableButton);
 downloadButton.addEventListener('click', () => {
   const downloadUrl = 'https://storage.cloud.google.com/co2_app/GPP_CTE_ST_NT_ML/COG/gpp_cte_st_nt_ml_2020_cog.tif'; // replace with your signed URL
   const downloadAnchor = document.createElement('a');
