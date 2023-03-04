078956785590e97f94032fdde0e5bc35bc0fc42b
Sybil Santos-Burgan <ssburgan@gmail.com>
2023-03-04 16:57:23 -0600

    update data download js

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,10 +14,41 @@ const selectedModel = document.querySelector( 'input[name="model"]:checked');
 
 //downloadButton.addEventListener('click', disableButton);
 downloadButton.addEventListener('click', () => {
-  const downloadUrl = 'https://storage.cloud.google.com/co2_app/GPP_CTE_ST_NT_ML/COG/gpp_cte_st_nt_ml_2020_cog.tif'; // replace with your signed URL
+
+  //Cedar collection
+  var uriBaseCedarCFE = 'gs://co2_app/GPP_CTE_ST_NT_BASE/COG/';
+  var uriBaseCedarML = 'gs://co2_app/GPP_CTE_ST_NT_ML/COG/';
+  var uriBaseCedarHYBRID = 'gs://co2_app/GPP_CTE_ST_NT_HYBRID/COG/';
+  var downloadUrl = 'https://storage.cloud.google.com/co2_app/MODEL/COG/gpp_cte_st_nt_ml_YEAR_cog.tif';
+
+  var selectedYear = $("#select-year").val();
+  var selectedModel = $('input[name="model"]:checked').val();
+
+  downloadUrl = downloadUrl.replace("MODEL", selectedModel);
+  downloadUrl = downloadUrl.replace("YEAR", selectedYear);
+  //const downloadUrl = 'https://storage.cloud.google.com/co2_app/GPP_CTE_ST_NT_ML/COG/gpp_cte_st_nt_ml_2020_cog.tif'; // replace with your signed URL
   const downloadAnchor = document.createElement('a');
   downloadAnchor.setAttribute('href', downloadUrl);
-  downloadAnchor.setAttribute('target', '_blank');
+  //downloadAnchor.setAttribute('target', '_blank');
   downloadAnchor.setAttribute('download', ''); // empty string sets the filename to the original filename
   downloadAnchor.click();
 });
+
+    // Check the radio button value.
+    function modelselect(model) {
+      var selectedYear = $("#select-year").val();
+      if (selectedYear != "" && model != "") {
+        $("#download-button").prop("disabled", false);
+      } else {
+        $("#download-button").prop("disabled", true);
+      }
+    }
+
+    function selectyear(year) {
+      var selectedYear = $("#select-year").val();
+      if (selectedYear != "" && year != "") {
+        $("#download-button").prop("disabled", false);
+      } else {
+        $("#download-button").prop("disabled", true);
+      }
+    }
