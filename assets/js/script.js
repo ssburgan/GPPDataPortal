d53ab07fa52c2a2338f3cb69776a9d81fccb0fa9
Sybil Santos-Burgan <ssburgan@gmail.com>
2023-03-03 22:51:03 -0600

    site cleanup

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// function redirectToGoogle() {
+//     window.location.href = "https://www.google.com";
+//   }
+  
+// var googleButton = document.getElementById("google-button");
+// googleButton.onclick = redirectToGoogle;
+
+
+
+const downloadButton = document.getElementById('download-button');
+
+downloadButton.addEventListener('click', () => {
+  const downloadUrl = 'https://storage.cloud.google.com/co2_app/GPP_CTE_ST_NT_ML/COG/gpp_cte_st_nt_ml_2020_cog.tif'; // replace with your signed URL
+  const downloadAnchor = document.createElement('a');
+  downloadAnchor.setAttribute('href', downloadUrl);
+  downloadAnchor.setAttribute('download', ''); // empty string sets the filename to the original filename
+  downloadAnchor.click();
+});
