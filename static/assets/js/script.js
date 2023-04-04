e81e7e92491d987952674c949db0a6533d3a3387
Sybil Santos-Burgan <ssburgan@gmail.com>
2023-04-03 21:37:29 -0500

    update script and remove analysis

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -5,8 +5,32 @@ const downloadButton = document.getElementById('download-button');
 const selectYear = document.getElementById('select-year');
 const selectedModel = document.querySelector( 'input[name="model"]:checked');   
 
-downloadButton.addEventListener('click', () => {
+// downloadButton.addEventListener('click', () => {
 
+//   //Cedar collection
+//   var downloadUrl;
+//   var baseUrl = 'https://storage.googleapis.com/co2_app/GPP_CTE_ST_NT_BASE/GPP_CTE_ST_NT_BASE.zip?authuser=0'
+//   var hybridUrl = 'https://storage.googleapis.com/co2_app/GPP_CTE_ST_NT_HYBRID/GPP_CTE_ST_NT_HYBRID.zip?authuser=0'
+//   var mlUrl = 'https://storage.googleapis.com/co2_app/GPP_CTE_ST_NT_ML/GPP_CTE_ST_NT_ML.zip?authuser=0'
+  
+//   var selectedModel = $('input[name="model"]:checked').val();
+//   downloadUrl = baseUrl;
+//   if(selectedModel =='GPP_CTE_ST_NT_ML'){
+//     downloadUrl = mlUrl;
+//   } else if(selectedModel =='GPP_CTE_ST_NT_HYBRID'){
+//     downloadUrl = hybridUrl;
+//   } 
+
+
+//   const downloadAnchor = document.createElement('a');
+//   downloadAnchor.setAttribute('href', downloadUrl);
+//   //downloadAnchor.setAttribute('target', '_blank');
+//   downloadAnchor.setAttribute('download', ''); // empty string sets the filename to the original filename
+//   downloadAnchor.click();
+// });
+
+function downloadDataset(){
+  
   //Cedar collection
   var downloadUrl;
   var baseUrl = 'https://storage.googleapis.com/co2_app/GPP_CTE_ST_NT_BASE/GPP_CTE_ST_NT_BASE.zip?authuser=0'
@@ -27,7 +51,7 @@ downloadButton.addEventListener('click', () => {
   //downloadAnchor.setAttribute('target', '_blank');
   downloadAnchor.setAttribute('download', ''); // empty string sets the filename to the original filename
   downloadAnchor.click();
-});
+}
 
 function enableDownload(){
   var selectedModel = $('input[name="model"]:checked').val();
